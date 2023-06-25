7bc562b6ef8391f8fef7f65cd2106df8758ea682
vika-tsymbaliyk <vika.bronytska@gmail.com>
2023-06-25 12:40:48 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector('.gallery');
+const listContent = generateGalleryList(galleryItems);
+
+list.insertAdjacentHTML("beforeend", listContent);
+
+function generateGalleryList(galleryItems) {
+    return galleryItems.flatMap((image) => `<li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src=${image.preview}
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</li>`)
+  .join("");
+}
